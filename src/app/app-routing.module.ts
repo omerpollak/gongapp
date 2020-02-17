2265aa4518004da63c9c90daae7adc00c9660096
Omer Pollak <omer_p@optimove.com>
2020-02-17 14:11:52 +0200

    adding the editor component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { EditorComponent } from './editor/editor.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'editor', component: EditorComponent },
   { path: '',   redirectTo: '/login', pathMatch: 'full' },
 ];
 
